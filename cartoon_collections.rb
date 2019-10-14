8c62181a5905de21bb9c3549eff43e206bd5e020
Michael Z <mziacm@gmail.com>
2019-10-14 03:44:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index{|names, i| puts "#{i+1}: #{names}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planet_calls)
+  planet_calls.map! {|planet_calls| planet_calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_calls)
+  #iterate through array
+  #for each element in array, find length of that element
+  ##if element length > 4
+  ###return true
+  ##else
+  ###return false
 end
 
 def find_the_cheese# code an argument here
