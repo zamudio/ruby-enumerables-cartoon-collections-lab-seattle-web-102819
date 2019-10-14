418b53f298119a84694c5b7f2d1b40e46517276e
Michael Z <mziacm@gmail.com>
2019-10-14 04:40:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def long_planeteer_calls(long_calls)
 end
 
 def find_the_cheese(ingredients)
+  ingredients.include?("cheddar") || ingredients.include?("gouda") || ingredients.include?("camembert")
   
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
